f11384b33709c9b1f60f5d94b2cb87a03d23c0d7
Rodrigo <drigodeveloper@gmail.com>
2023-05-23 22:54:51 -0300

    Error, update não funcionou

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -22,6 +22,39 @@ class UsersController {
         return response.status(201).json();
     }
     
+
+    async update(request, response) {
+        const { name, email } = request.body;
+        const { id } = request.params;
+
+        const database = await sqliteConecition();
+        const user = await database.get("SELECT * FROM users WHERE ID = (?)", [id]);
+
+        if(!user) {
+            throw new AppError("Usuário não encontrado");
+        }
+
+        const userWithUpdatedEmail = database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+            throw new AppError("Este e-mail ja esta em uso");
+        }
+
+        user.name = name;
+        user.email = email;
+
+        await database.run(`
+        UPDATE users SET,
+        name = ?,
+        email = ?,
+        updated_at = ?,
+        WHERE id = ?`
+        [user.name, user.email, new Date(), id]
+        );
+
+        return response.json()
+        //Esse trecho do código poderia ser response.status(200).json() mas por padrão essa resposta retorna o status 200 então podemos ocultar essa parte
+        }
 };
 
 module.exports = UsersController
